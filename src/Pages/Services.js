45b77922e2d5b4972129c443dea33927c1a9434c
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-06-29 16:00:55 -0400

    working services

---
+++ src/Pages/Services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Services() {
+    return (
+        <div>
+            <div>
+                <h2>Our Services for You</h2>
+            </div>
+            <div>
+                <p>
+                Highlight the best of your properties by using the List Category shortcode. You can list categories, types, cities, areas and states of your choice.
+                </p>
+            </div>
+        </div>
+    )
+}
