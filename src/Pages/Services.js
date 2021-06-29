d39b5cd909c54f39184849f96ced5a9c87dab7fd
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-06-29 16:20:41 -0400

    services

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React from "react";
 
 export default function Services() {
-    return (
-        <div>
-            <div>
-                <h2>Our Services for You</h2>
-            </div>
-            <div>
-                <p>
-                Highlight the best of your properties by using the List Category shortcode. You can list categories, types, cities, areas and states of your choice.
-                </p>
-            </div>
+  return (
+    <div className="services">
+      <div className="services__title">
+        <h2>Our Services for You</h2>
+      </div>
+      <div className="services__description">
+        <p>
+          Highlight the best of your properties by using the List Category
+          shortcode. You can list categories, types, cities, areas and states of
+          your choice.
+        </p>
+      </div>
+      <div className="services__content">
+        <div className="services--buyhome">
+          <img src="" alt="" />
+          <h3>
+            <strong>BUY HOME</strong>
+          </h3>
+          <p>
+            Buying a home doesn’t have to be an overwhelming process when you
+            have the right professional by your side. Let’s find one you’ll
+            love.
+          </p>
         </div>
-    )
+        <div className="services--sellhome">
+          <img src="" alt="" />
+          <h3>
+            <strong>SELL HOME</strong>
+          </h3>
+          <p>
+            Do you know what your home is truly worth in our market? If you
+            price too high, you won’t sell. If you price too low, you could be
+            losing tens of thousands.
+          </p>
+        </div>
+        <div className="services--localexpertise">
+          <img src="" alt="" />
+          <h3>
+            <strong>LOCAL EXPERTISE</strong>
+          </h3>
+          <p>
+            No matter your real estate needs, as a local expert in our market, I
+            can assist! Whether you’re staying in town or moving out of state,
+            I’ll simplify the process.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
