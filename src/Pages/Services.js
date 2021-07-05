4e72a06f1306d86108e0dc0d71520de5532b88f3
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-05 18:05:31 -0400

    aboutme update styles 💄

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -1,12 +1,11 @@
 import React from "react";
+import "../assets/styles/Components/Services.scss"
 
 export default function Services() {
   return (
     <div className="services">
       <div className="services__title">
         <h2>Our Services for You</h2>
-      </div>
-      <div className="services__description">
         <p>
           Highlight the best of your properties by using the List Category
           shortcode. You can list categories, types, cities, areas and states of
