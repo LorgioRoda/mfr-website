ded8b37ec8c8b27fd4977d9051e0e984f3a14d1a
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-07 18:24:16 -0400

    created initial state with recommend

---
+++ src/Pages/Recommendation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+export default function Recommendation() {
+    return (
+        <div className="recommendation">
+            <div className="recommendation__card">
+                <div className="card--title">
+                    <div className="card--image">
+                        <img src="" alt=""/>
+                            <div className="card--start">
+                                <div className="card--content">
+
+                                </div>
+                            </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    )
+}
