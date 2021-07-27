dfb70c3dbd7bc7d821b2591b809331eccb227911
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-27 17:13:11 -0400

    fix bug webpack dev-server 🐛

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -10,16 +10,18 @@ export default function Card() {
           <div className="card--start">
          </div>
             <img src={starts} alt="" />
-            <p>
-              In just two days of visiting apartments we found the perfect place
-              for me, and under my budget! Fernanda got what I wanted very quickly
-              and, unlike other realtors in the past, she only showed me
-              apartments within my budget that I would be interested in. She was
-              always very responsive, friendly, and willing to go the extra mile.
-              She also helped my sister, who lived our of state, find her perfect
-              place. I've already recommended her to everyone I know is looking
-              for a place!
-            </p>
+            <div className="card--content">
+              <p>
+                In just two days of visiting apartments we found the perfect place
+                for me, and under my budget! Fernanda got what I wanted very quickly
+                and, unlike other realtors in the past, she only showed me
+                apartments within my budget that I would be interested in. She was
+                always very responsive, friendly, and willing to go the extra mile.
+                She also helped my sister, who lived our of state, find her perfect
+                place. I've already recommended her to everyone I know is looking
+                for a place!
+              </p>
+            </div>
         </div>
       </div>
     </>
