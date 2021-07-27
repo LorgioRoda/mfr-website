6478eb584d317f882bafb4f1c8f78641a93d1332
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-27 16:54:09 -0400

    styles card 💄

--- src/Pages/Recommendation.js
+++ src/Pages/Recommendation.js
@@ -7,23 +7,25 @@ import "../assets/styles/Components/Recommendations.scss";
 export default class Recommendation extends Component {
   render() {
     return (
-      <Carousel
+/*       <Carousel
         showArrows={true}
         InfiniteLoop={true}
         showThumbs={false}
         showThumbs={false}
         autoPlay={true}
         interval={3100}
-      >
+      /> */
+      <>
+      <div className="recommendation--title">
        <h2>Clients Testimonials</h2>
         <p>
           Highlight the best of your properties by using the List Category
           shortcode. You can list categories, types, cities, areas and states of
           your choice.
         </p>
-      <Card/>
-      <Card/>
-      </Carousel>
+      </div>
+        <Card/>
+      </>
     );
   }
 }
