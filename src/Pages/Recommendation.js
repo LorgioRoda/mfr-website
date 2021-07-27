181594e97893d861f192d19a9b254cb1a4e0fcbe
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-27 17:47:19 -0400

    carousel is worki done

--- src/Pages/Recommendation.js
+++ src/Pages/Recommendation.js
@@ -2,28 +2,36 @@ import React, { Component } from "react";
 import { Carousel } from "react-responsive-carousel";
 import Card from "../Components/Card";
 import "../assets/styles/Components/Recommendations.scss";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default class Recommendation extends Component {
   render() {
     return (
-      <Carousel
-        showArrows={true}
-        InfiniteLoop={true}
-        showThumbs={false}
-        showThumbs={false}
-        autoPlay={true}
-        interval={100}
-      >
-        <div>
-          <div className="recommendation--title">
-            <h2>Clients Testimonials</h2>
-          </div>
-            <Card/>
-            <Card/>
-            <Card/>
+      <>
+        <div className="recommendation--title">
+          <h2>Clients Testimonials</h2>
         </div>
-      </Carousel>
+        <Carousel
+          showArrows={true}
+          onChange={true}
+          onClickItem={true}
+          InfiniteLoop={true}
+          showThumbs={false}
+          showThumbs={false}
+          autoPlay={true}
+          interval={1500}
+        >
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+        </Carousel>
+      </>
     );
   }
 }
