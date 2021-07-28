88927952c9fe1d3e8396bc9a590ef0fafde24f8b
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-28 11:14:42 -0400

    working on page local updates

--- src/container/App.js
+++ src/container/App.js
@@ -4,7 +4,6 @@ import AboutMe from "../Pages/AboutMe"
 import Services from '../Pages/Services'
 import Recommendation from "../Pages/Recommendation"
 
-
 export default function app() {
     return (
         <div className="App">
