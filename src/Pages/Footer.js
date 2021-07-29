428e27d1aed9948cc84b035f2afdb3dc310b42ab
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-29 19:16:26 -0400

    fix bug testimonial 🐛

---
+++ src/Pages/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <li className="footer__contact">
+                <h4>Contact</h4>
+                <p><i></i></p>
+                <p><i></i></p>
+                <p><i></i></p>
+                <p><i></i></p>
+            </li>
+        </div>
+    )
+}
--- src/container/App.js
+++ src/container/App.js
@@ -4,6 +4,7 @@ import AboutMe from "../Pages/AboutMe"
 import Services from '../Pages/Services'
 import Recommendation from "../Pages/Recommendation"
 import LocalUpdates from '../Pages/LocalUpdates'
+import Footer from '../Pages/Footer'
 
 export default function app() {
     return (
@@ -13,6 +14,7 @@ export default function app() {
             <Services/>
             <Recommendation/>
             <LocalUpdates/>
+            <Footer/>
         </div>
     )
 }
