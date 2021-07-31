d0c1c600ed96379dd0ca2f70a87f02570633bfe1
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-07-31 12:09:33 -0400

    working on styles footer

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,27 +1,16 @@
 import React from "react";
+import "../assets/styles/Components/Footer.scss";
 import logo_facebook from "../assets/static/pink_facebook.svg";
-import logo_instagram from "../assets/static/pink_instagram.svg"
-import logo_building from "../assets/static/pink_building.svg"
+import logo_instagram from "../assets/static/pink_instagram.svg";
+import logo_building from "../assets/static/pink_building.svg";
 import logo_email from "../assets/static/pink_email.svg";
 import logo_phone from "../assets/static/pink_wpp.svg";
-import newyorkcasas from "../assets/static/nycasas-logo-black-small.png"
+import newyorkcasas from "../assets/static/nycasas-logo-black-small.png";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <li className="footer__contact">
-        <h4>Contact me</h4>
-        <p>
-        <img src={logo_building} alt=""/>745 FIFTH AVENUE, SUITE 500 NEW YORK CITY, NY 10151-0099, USA
-        </p>
-        <p>
-          <img src={logo_phone} alt=""/> 48585840394
-        </p>
-        <p>
-        <img src={logo_email} alt=""/>fernanda@nycasas.com
-        </p>
-      </li>
-      <li>
+      <div>
         <div className="footer__social">
           <h4>
             Maria <span>Fernanda</span> Roda
@@ -47,10 +36,30 @@ export default function Footer() {
             <img src={logo_facebook} alt="" />
           </a>
         </div>
-        <div className="footer__sponsor">
-          <a href="https://nycasas.com/team/maria-fernanda-roda.php" target="_blank" rel="noopener noreferrer"><img src={newyorkcasas} alt="" /></a>
-        </div>
-      </li>
+      </div>
+      <div className="footer__contact">
+        <h4>Contact me</h4>
+        <p>
+          <img src={logo_building} alt="" />
+          745 FIFTH AVENUE, SUITE 500 NEW YORK CITY, NY 10151-0099, USA
+        </p>
+        <p>
+          <img src={logo_phone} alt="" /> 48585840394
+        </p>
+        <p>
+          <img src={logo_email} alt="" />
+          fernanda@nycasas.com
+        </p>
+      </div>
+      <div className="footer__sponsor">
+        <a
+          href="https://nycasas.com/team/maria-fernanda-roda.php"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={newyorkcasas} alt="" />
+        </a>
+      </div>
     </div>
   );
 }
