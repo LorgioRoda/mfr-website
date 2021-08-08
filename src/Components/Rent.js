df90cecbda0bfa5b8760efe3db1a8a43a4c09b91
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-08 19:05:13 -0400

    working on rents jsx completed

--- src/Components/Rent.js
+++ src/Components/Rent.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import rent507 from "../assets/static/rent_507west.png"
+import bed from "../assets/static/bed.svg";
+import relax from "../assets/static/relax.svg";
+import bath from "../assets/static/bath.svg";
 
 export default function Rent() {
     return (
-        <div>
-            
+        <div className="rent">
+            <button>Rentals</button>
+            <img src={rent507} alt=""/>
+            <div className="rent__container">
+                <h4>Title</h4>
+                <p><span>$ 2313 / month</span></p>
+                <p>Come live in this LUXURY FULL service building with 24-hour doorman & amazing amenities.</p>
+                <div className="rent__icons">
+                    <img src={bed} alt=""/>
+                    <p>2</p>
+                    <img src={relax} alt=""/>
+                    <p>2</p>
+                    <img src={bath} alt=""/>
+                    <p>2</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Pages/Listings.js
+++ src/Pages/Listings.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Rent from '../Components/Rent'
 
 export default function Listings() {
     return (
         <>
             <h3>Our listings</h3>
             <p>Showcase properties in homepage to be visible and accessible. Select the most wanted categories or cities to be displayed in the lists.</p>
+            <Rent/>
         </>
     )
 }
