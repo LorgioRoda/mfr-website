5a5966e8db61b6e4e394b5c6ce227d2214f286da
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-12 20:22:24 -0400

    carts jsx DONE

--- src/Pages/Listings.js
+++ src/Pages/Listings.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import Rent from '../Components/Rent'
+import React from "react";
+import Rent from "../Components/Rent";
 
 export default function Listings() {
-    return (
-        <>
-            <h3>Our listings</h3>
-            <p>Showcase properties in homepage to be visible and accessible. Select the most wanted categories or cities to be displayed in the lists.</p>
-            <Rent/>
-        </>
-    )
+  return (
+    <>
+      <div className="">
+        <h3>Our listings</h3>
+        <p>
+          Showcase properties in homepage to be visible and accessible. Select
+          the most wanted categories or cities to be displayed in the lists.
+        </p>
+      </div>
+      <Rent />
+    </>
+  );
 }
