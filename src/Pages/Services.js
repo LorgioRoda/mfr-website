9f4c50838c93af81cf6ee50e9f5df7d5d1eb535e
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-15 15:14:53 -0400

    💄responsive desing complated

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -10,39 +10,41 @@ export default function Services() {
       <div className="services__title">
         <h2>Our Services for You</h2>
       </div>
-        <div className="services--buyhome">
-          <img src={rent}  alt="" />
-          <h3>
-            <strong>Buy a home with me</strong>
-          </h3>
-          <p>
-            Buying a home doesn’t have to be an overwhelming process when you
-            have the right professional by your side.
-            Let’s find one you’ll
-            love.
-          </p>
-        </div>
-        <div className="services--sellhome">
-          <img src={forRent} alt="" />
-          <h3>
-            <strong>SELL HOME</strong>
-          </h3>
-          <p>
-            Do you know what your home is truly worth in our market? If you
-            price too high, you won’t sell. If you price too low, you could be
-            losing tens of thousands.
-          </p>
-        </div>
-        <div className="services--localexpertise">
-          <img src={rentExpertise}alt="" />
-          <h3>
-            <strong>LOCAL EXPERTISE</strong>
-          </h3>
-          <p>
-            No matter your real estate needs, as a local expert in our market, I
-            can assist! Whether you’re staying in town or moving out of state,
-            I’ll simplify the process.
-          </p>
+        <div className="services__content">
+          <div className="services--buyhome">
+            <img src={rent}  alt="" />
+            <h3>
+              <strong>Buy a home with me</strong>
+            </h3>
+            <p>
+              Buying a home doesn’t have to be an overwhelming process when you
+              have the right professional by your side.
+              Let’s find one you’ll
+              love.
+            </p>
+          </div>
+          <div className="services--sellhome">
+            <img src={forRent} alt="" />
+            <h3>
+              <strong>SELL HOME</strong>
+            </h3>
+            <p>
+              Do you know what your home is truly worth in our market? If you
+              price too high, you won’t sell. If you price too low, you could be
+              losing tens of thousands.
+            </p>
+          </div>
+          <div className="services--localexpertise">
+            <img src={rentExpertise}alt="" />
+            <h3>
+              <strong>LOCAL EXPERTISE</strong>
+            </h3>
+            <p>
+              No matter your real estate needs, as a local expert in our market, I
+              can assist! Whether you’re staying in town or moving out of state,
+              I’ll simplify the process.
+            </p>
+          </div>
         </div>
       </div>
   );
