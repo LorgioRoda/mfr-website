e1d883fe4a321e20f5d8be6abdd8050f9d4fe9cc
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-17 14:14:10 -0400

    validation on form DONE

--- src/Pages/ContactMe.js
+++ src/Pages/ContactMe.js
@@ -7,9 +7,9 @@ const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
 const regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 const validation = {
-  name: (value) => {
+  name: (value ) => {
     let alert;
-    if (!value) {
+    if (!value || regName) {
       alert = "Name is requiered and only use words";
     } else if (value.length < 3) {
       alert = "Name have to have more 3 words";
@@ -18,7 +18,7 @@ const validation = {
   },
   email: (value) => {
     let alert;
-    if (!value) {
+    if (!value || regEmail) {
       alert = "Email is required";
     }
     return alert;
