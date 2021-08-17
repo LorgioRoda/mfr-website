1b6eb742c6369f13523cb86a7970ac1964735e7c
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-17 15:14:59 -0400

    add a component Form

---
+++ src/Components/Form.js
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import "../assets/styles/Components/ContactMe.scss"
+
+//value with regex
+const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+const regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+// Validation on Form
+const validation = {
+  name: (value ) => {
+    let alert;
+    if (!value || !regName) {
+      alert = "Name is requiered and only use words";
+    } else if (value.length < 3) {
+      alert = "Name have to have more 3 words";
+    }
+    return alert;
+  },
+  email: (value) => {
+    let alert;
+    if (!value || !regEmail) {
+      alert = "Email is required";
+    }
+    return alert;
+  },
+};
+export default function Form() {
+
+  const formContact = { name: "", email: "", message: "" };
+  const errors = { name: null, email: null };
+
+  const [contact, setContact] = useState(formContact);
+  const [stateErrors, setStateErrors] = useState(errors);
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setContact({ ...contact, [name]: value });
+    setStateErrors({ ...stateErrors, [name]: validation[name](value) });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(isValid()){
+        emailjs.sendForm(
+          `${process.env.KEYSERVICE}`,
+          `${process.env.KEYTEMPLATE}`,
+          e.target,
+          `${process.env.KEYUSER}`
+        );
+        setContact(formContact);
+        alert('Funciona')
+    }else if(!isValid()){
+        
+    }
+};
+
+  const isValid = () => {
+    return !Object.keys(stateErrors).some((key) => stateErrors[key] !== undefined);
+  };
+
+  return (
+    <>
+      <div className="content">
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form__content">
+            <h3>
+              Arrange a meeting with Maria Fernanda to buy, sell or rent your
+              home.
+            </h3>
+            <input
+              name="name"
+              placeholder="Your name*"
+              type="text"
+              onChange={(event) => handleChange(event)}
+              value={contact.name}
+            />
+            {stateErrors.name && <span>{stateErrors.name}</span>}
+            <div className="form__content--message">
+              <input
+                name="email"
+                placeholder="Your email*"
+                type="email"
+                onChange={(event) => handleChange(event)}
+                value={contact.email}
+              />
+              {stateErrors.email && <span>{stateErrors.email}</span>}
+              <textarea
+                name="message"
+                placeholder="type your message"
+                type="text"
+                onChange={(event) => handleChange(event)}
+                value={contact.message}
+              />
+            </div>
+            <div className="form_content--buttom">
+              <input type="submit" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
---
+++ src/Components/ModalForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ModalForm() {
+    return (
+        <>
+            <div className="modalForm">
+                <h3>Thanks, your form was send</h3>
+            </div>
+        </>
+    )
+}
