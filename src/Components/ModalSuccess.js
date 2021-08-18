5b6e74c6576de33692e36717fe47cc4a4523a06a
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-18 15:05:39 -0400

    working on animation

--- src/Components/ModalSuccess.js
+++ src/Components/ModalSuccess.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
-import Portal from "./Portal"
+import React, { Component } from "react";
+import Portal from "./Portal";
+import "../assets/styles/Components/ModalSuccess.scss";
 
 export default class ModalSuccess extends Component {
-    render() {
-        const {children, toggle, active} = this.props
-        return (
-            <Portal>
-                {active && (<div>
-                    <h3>thanks for you message</h3>
-                    <button onClick={toggle}></button>
-                    <div>{children}</div>
-                </div>)}
-            </Portal>
-        )
-    }
+  render() {
+    const { children, toggle, active } = this.props;
+    return (
+      <Portal>
+        {active && (
+          <div className="modal">
+            <button onClick={toggle}>❌</button>
+            <h3>You form was send</h3>
+            <div class="success-checkmark">
+              <div class="check-icon">
+                <span class="icon-line line-tip"></span>
+                <span class="icon-line line-long"></span>
+                <div class="icon-circle"></div>
+                <div class="icon-fix"></div>
+              </div>
+            </div>
+            <div>{children}</div>
+          </div>
+        )}
+      </Portal>
+    );
+  }
 }
-
