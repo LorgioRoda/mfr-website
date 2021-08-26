8f5cac9a1d1a96d88155e9e761c197043d64d68a
Lorgio Roda <lorgiorodaroca@gmail.com>
2021-08-26 11:24:51 +0200

    footer styles DONE 💄

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -20,25 +20,28 @@ export default function Footer() {
           </h4>
           <div className="footer__social__content">
             <NewLetterForm />
-            <a
-              href="https://www.instagram.com/mariafernandaroda/?hl=en"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={logo_instagram} alt="" />
-            </a>
-            <a
-              href="https://www.facebook.com/newyorkrealestatemfr"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={logo_facebook} alt="" />
-            </a>
+            <h3>Follow me</h3>
+            <div className="footer__social--media">
+              <a
+                href="https://www.instagram.com/mariafernandaroda/?hl=en"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={logo_instagram} alt="Instagram" />
+              </a>
+              <a
+                href="https://www.facebook.com/newyorkrealestatemfr"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={logo_facebook} alt="Facebook" />
+              </a>
+            </div>
           </div>
       </div>
       <div className="footer__contact">
         <h4>Contact Me</h4>
-        <div className="">
+        <div className="footer__contact--building">
           <a href="https://www.google.com/maps/place/745+5th+Ave+suite+500,+New+York,+NY+10151,+USA/@40.7631646,-73.9773235,17z/data=!3m1!4b1!4m5!3m4!1s0x89c258fa6e49df93:0x15b3992ff3bb4a60!8m2!3d40.7631646!4d-73.9751348" target="_blank" rel="noopener noreferrer">
               <img src={logo_building} alt="" />
           </a>
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, IndexRoute } from 'react-router-dom';
 import Home from "../Pages/Home"
 import Services from '../Pages/Services'
 import Recommendation from "../Pages/Recommendation"
@@ -6,16 +7,17 @@ import Footer from '../Pages/Footer'
 import ContactMe from '../Pages/ContactMe'
 import Listings from '../Pages/Listings'
 import initialState from "../initialState"
+import NotFound from '../Pages/NotFound';
 
 export default function app() {
     return (
         <>
             <Home/>
-            <Listings/>
-            <Services/>
-            <Recommendation recommendations={initialState.recommendations}/>
-            <ContactMe/>
-            <Footer/>
+            <Listings />
+            <Services />
+            <Recommendation component={Recommendation} recommendations={initialState.recommendations}/>
+            <ContactMe />
+            <Footer />
         </>
     )
 }
